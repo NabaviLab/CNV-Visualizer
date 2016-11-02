75c1d1620ded2d612d34a021e9e2dc3508fe579f
Denis <denisv.ct@gmail.com>
2016-11-01 22:13:16 -0400

    Added log_ratios route and jsonparser.py

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,14 +1,26 @@
 from eve import Eve
+from json import load
+from json import loads
+from .jsonparser import jparse
 
 app = Eve()
 
 @app.route('/cnvvis')
 def base():
-	return 'hello'
+        return 'hello'
 
 @app.route('/cnvvis/<name>')
 def greet(name):
-	return "hi " + name
+        return "hi " + name
+
+@app.route('/cnvvis/logratios/<start>-<stop>')
+	def logs(start, stop):
+        #add method call to get .txt file of section from galaxy (returns path)
+        jpath = jparse(<path to .txt_file>)
+        with open(jpath) as jfile
+	        jstring = load(jfile)
+        jobj = loads(jstring)
+        return jobj
 
 if __name__=="__main__":
 	app.run()
---
+++ server/app/jsonparser.py
@@ -0,0 +1,35 @@
+from json import dump
+import re
+
+def jparse(path):
+	outpath = path
+        outpath = outpath[:-4]
+        outpath = outpath.join('.json')
+        jtxt = open(path,'r')
+        lines = jtxt.readlines()
+        jtxt.close()
+        linearray = []
+        linearray.append('{"chr1":[')
+        for index, value in enumerate(lines)
+	        if index != 0:
+                        string = value.split()
+                        linearray.append('{"chr_start":')
+                        linearray.append(string[1])
+                        linearray.append(',"chr_stop":')
+                        linearray.append(string[2])
+                        linearray.append(',"adjusted_log_ratio":')
+                        linearray.append(string[6])
+                        linearray.append(',"region_call":')
+                        linearray.append(string[8])
+                        linearray.append('"}')
+                        if (index+1) < len(lines):
+                                linearray.append(',')
+                        else:
+                                pass
+               else:
+                        pass
+        linearray.append(']}')
+        output = ''.join(linearray)
+        with open(outpath,'w') as outfile:
+                dump(output, outfile)
+        return outpath
