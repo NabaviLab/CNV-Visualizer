88c207aea5a8cefe3aaa5861d852673e058079bb
David Karwowski <karwowski.david@gmail.com>
2016-11-02 13:41:32 -0400

    Setting up galaxy communication

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,6 +1,5 @@
 from eve import Eve
-from json import load
-from json import loads
+from json import loads, load
 from .jsonparser import jparse
 
 app = Eve()
@@ -13,14 +12,14 @@ def base():
 def greet(name):
     return "hi " + name
 
-#@app.route('/cnvvis/logratios/<start>-<stop>')
-#def logs(start, stop):
-#    #add method call to get .txt file of section from galaxy (returns path)
-#    jpath = jparse(<path to .txt_file>)
-#    with open(jpath) as jfile
-#        jstring = load(jfile)
-#    jobj = loads(jstring)
-#    return jobj
+@app.route('/cnvvis/logratios/<start>-<stop>')
+def logs(start, stop):
+    #add method call to get .txt file of section from galaxy (returns path)
+    jpath = jparse('./data/filtered.called')
+    with open(jpath) as jfile:
+        jstring = load(jfile)
+        jobj = loads(jstring)
+        return jobj
 
 if __name__=="__main__":
     app.run()
---
+++ server/app/galaxy.py
@@ -0,0 +1,30 @@
+from bioblend import galaxy
+
+gi = galaxy.GalaxyInstance(url='http://127.0.0.1:8081', key=)
+dsc = galaxy.datasets.DatasetClient(gi)
+tlc = galaxy.tools.ToolClient(gi)
+hsc = galaxy.histories.HistoryClient(gi)
+
+library_id = gi.libraries.get_libraries(name="Test")[0]['id']
+library = [l for l in gi.libraries.show_library(library_id, contents=True) if l['type']=='file']
+
+tmp = hsc.create_history(name="CNV Vis")
+
+copynumber_id = [l['id'] for l in lib if 'copynumber' in l['name']]
+hsc.upload_dataset_form_library(tmp['id'], copynumber_id[0])
+base_id = hsc.show_history(tmp['id'], contents=True)[-1]['dataset_id']
+
+def get_filtered(start, stop):
+    global base_id, tlc, hsc, dsc
+    tool_inputs = {
+            'input': {
+                'src': 'hda',
+                'id': base_id
+                },
+            'header_lines': 1,
+            'cond': 'c2<' + str(stop) + ' and c3>' + str(start)
+            }
+    tlc.run_tool(tmp['id'], 'Filter1', tool_inputs)
+    filtered = hsc.show_history(tmp['id'], contents=True)[-1]
+    dsc.download_dataset(filtered['id'], file_path='./data/filtered.called', use_default_filename=False, wait_for_completion=True)
+    return './data/filtered.called'
