ccbe28852e49da5945086acc2e4ac960d4d2409d
David Karwowski <karwowski.david@gmail.com>
2017-04-27 13:26:30 -0400

    script now starts/stops the server

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-cd app/data
-python ../../scripts/cors_server.py &
-cd ../..
-electron app
+if [ $1 == "start" ]; then
+    cd app/data
+    python ../../scripts/cors_server.py &
+    echo $! > ../../cors_server.pid
+    cd ../..
+    electron app &
+else 
+    kill $(cat ./cors_server.pid)
+fi;
 
